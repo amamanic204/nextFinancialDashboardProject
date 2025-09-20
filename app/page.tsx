7bef32ed457f9743f33baef016e6b9f00a901cb4
amamanic204 <amamani204@umsa.bo>
2025-09-21 04:47:22 +0900

    solving errros caused by me not deleting a part that must have

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ import clsx from 'clsx';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-export default function Page({ status }: { status: string }) {
-  status = "paid";
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -15,7 +14,7 @@ export default function Page({ status }: { status: string }) {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.clasName}`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -23,14 +22,6 @@ export default function Page({ status }: { status: string }) {
             , brought to you by Vercel.
             
           </p>
-          <div className={clsx(
-          	'inline-flex items-center rounded-full px-2 py-1 text-sm', 
-		  	{
-		  		'bg-gray-100 text-gray-500': status === 'pending',
-		  		
-		  		'bg-green-500 text-white': status === 'paid',
-		  	},
-          	)} /> 
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
